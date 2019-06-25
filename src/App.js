a9c6522ff949a6b51b98e00d626910250c57360b
justin meskan <justinmeskan@gmail.com>
2019-06-25 13:45:24 -0700

    i just made m y first useState hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+  const [hookState, stateSetter] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <input type='text' value={hookState} onChange={e => stateSetter(e.target.value)}> 
+
+      </input>
+    </form>
   );
 }
 
